f73a27e1372aad9e07b2eba8ad2291a5d1e17117
Emiro-Osp <120336414+Emiro-Osp@users.noreply.github.com>
2023-02-22 22:26:12 -0500

    Update game-BM

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.getElementById("up");
+const btnDown = document.getElementById("down");
+const btnLeft = document.getElementById("left");
+const btnRight = document.getElementById("right");
 
 let canvasSize;
 let elementsSize;
@@ -17,22 +21,58 @@ function setCanvasSize (){
 canvas.setAttribute('width', canvasSize);
 canvas.setAttribute('height', canvasSize);
 
-let elementsSize = canvasSize / 10; 
+elementsSize = canvasSize / 10; 
 
 startGame();
 }
 
 function startGame (){
-    
 
-console.log({canvasSize, elementsSize});
+// console.log({canvasSize, elementsSize});
 
 game.font = elementsSize + 'px Verdana';
 game.textAlign = 'end';
 
-for (let i = 1; i <= 10; i++){
-game.fillText(emojis['X'], elementsSize , elementsSize * i);
+
+const map = maps[1];
+const mapRows = map.trim().split('\n');
+const mapRowsCol = mapRows.map(row => row.trim().split(''));
+console.log({map, mapRows, mapRowsCol});
+
+mapRowsCol.forEach((row, rowI )=> {
+    row.forEach((col, colI) => {
+        const emoji = emojis[col];
+        const posX =  elementsSize * (colI + 1);
+        const posY =  elementsSize * (rowI + 1);
+        game.fillText(emoji, posY, posX);
+    });
+});
+
+// for (let row = 1; row <= 10; row++) {
+//   for (let col = 1; col <= 10; col++) {
+//     game.fillText(emojis[mapRowsCol[row -1][col -1]], elementsSize * row, elementsSize * col);
+//         }
+//     }
 }
 
+window.addEventListener("keydown", event => console.log(event.key));
+btnUp.addEventListener("click", moveUp);
+btnDown.addEventListener("click", moveDown);
+btnLeft.addEventListener("click", moveLewft);
+btnRight.addEventListener("click", moveRight);
 
+function moveUp () {
+    console.log('up')
+}
+
+function moveDown () {
+    console.log('down')
+}
+
+function moveLewft () {
+    console.log('Left')
+}
+
+function moveRight () {
+    console.log('Right')
 }
